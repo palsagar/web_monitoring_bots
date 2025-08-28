5aea1736179e89b9b07dbdb11dc8b525fdcf00d0
Sagar Pal <sagar.pal@entalpic.ai>
2025-08-28 18:53:23 +0200

    Revert "fix: more timezone issues"
    This reverts commit 1cabc6d25b8498442191dd5ce824e1db4c5fc5b8.

--- src/web_monitoring_bots/browser_automation.py
+++ src/web_monitoring_bots/browser_automation.py
@@ -890,11 +890,11 @@ def main():
                 print("🔍 No changes detected in course offerings")
                 notification_manager.send_telegram(
                     message="No changes detected in course offerings"
-                    + f"\n{get_current_time()}",
+                    + f"\n{cached_content['timestamp']}",
                 )
                 notification_manager.send_discord(
                     message="No changes detected in course offerings"
-                    + f"\n{get_current_time()}",
+                    + f"\n{cached_content['timestamp']}",
                 )
                 monitor.save_cached_content(combined_string, current_hash)
     finally:
